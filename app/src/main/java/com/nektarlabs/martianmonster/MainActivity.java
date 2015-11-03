472f2a781479baf272393adf716d3e6545dad716
vik denic <vik.denic@gmail.com>
2015-11-03 13:41:16 -0600

    Play sound for associated button

--- app/src/main/java/com/nektarlabs/martianmonster/MainActivity.java
+++ app/src/main/java/com/nektarlabs/martianmonster/MainActivity.java
@@ -1,9 +1,10 @@
 package com.nektarlabs.martianmonster;
 
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -22,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5 })
     public void onSoundboardButtonClicked(View view) {
+        int resid = residForButton(view);
+
         stopPlaying();
-        mMediaPlayer = MediaPlayer.create(this, R.raw.firstastronaut);
+        mMediaPlayer = MediaPlayer.create(this, resid);
         mMediaPlayer.start();
     }
 
@@ -33,5 +36,31 @@ public class MainActivity extends AppCompatActivity {
             mMediaPlayer.release();
             mMediaPlayer = null;
         }
+    }
+
+    private int residForButton(View view) {
+        Button button = (Button) view;
+        int resid;
+
+        switch(button.getId()) {
+            case R.id.button1:
+                resid = R.raw.firstastronaut;
+                break;
+            case R.id.button2:
+                resid = R.raw.incrediblespeed;
+                break;
+            case R.id.button3:
+                resid = R.raw.tripisshort;
+                break;
+            case R.id.button4:
+                resid = R.raw.ufo;
+                break;
+            case R.id.button5:
+                resid = R.raw.blastoff;
+                break;
+            default:
+                return R.raw.firstastronaut;
+        }
+        return resid;
     }
 }
