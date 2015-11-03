7699049d0b893eb75176276d45d66a1b9312ae06
vik denic <vik.denic@gmail.com>
2015-11-03 13:11:01 -0600

    Play soundbite for first button

--- app/src/main/java/com/nektarlabs/martianmonster/MainActivity.java
+++ app/src/main/java/com/nektarlabs/martianmonster/MainActivity.java
@@ -1,13 +1,37 @@
 package com.nektarlabs.martianmonster;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button1)
+    public void playSound(View view) {
+        stopPlaying();
+        mMediaPlayer = MediaPlayer.create(this, R.raw.firstastronaut);
+        mMediaPlayer.start();
+    }
+
+    private void stopPlaying() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
     }
 }
