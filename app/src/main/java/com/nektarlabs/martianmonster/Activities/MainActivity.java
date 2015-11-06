dd4997e5ebf223538a67ee8b962c9a3a7a712b68
vik denic <vik.denic@gmail.com>
2015-11-06 14:47:58 -0600

    move Fabric initialization to Applciation class

--- app/src/main/java/com/nektarlabs/martianmonster/Activities/MainActivity.java
+++ app/src/main/java/com/nektarlabs/martianmonster/Activities/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
--- app/src/main/java/com/nektarlabs/martianmonster/Application/MMApplication.java
+++ app/src/main/java/com/nektarlabs/martianmonster/Application/MMApplication.java
@@ -2,8 +2,11 @@ package com.nektarlabs.martianmonster.Application;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.nektarlabs.martianmonster.R;
 import com.parse.Parse;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by viktordenic on 11/6/15.
@@ -12,6 +15,8 @@ public class MMApplication extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
 
         Parse.initialize(this, getString(R.string.parse_application_id), getString(R.string.parse_client_secret));
     }
