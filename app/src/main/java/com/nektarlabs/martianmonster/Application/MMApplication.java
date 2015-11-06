ef1280f3f7e27ce38115463b44bfb460be8abd33
vik denic <vik.denic@gmail.com>
2015-11-06 15:15:44 -0600

    Create Parse user

--- app/src/main/java/com/nektarlabs/martianmonster/Application/MMApplication.java
+++ app/src/main/java/com/nektarlabs/martianmonster/Application/MMApplication.java
@@ -5,6 +5,11 @@ import android.app.Application;
 import com.crashlytics.android.Crashlytics;
 import com.nektarlabs.martianmonster.R;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.util.UUID;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -19,5 +24,25 @@ public class MMApplication extends Application {
         Fabric.with(this, new Crashlytics());
 
         Parse.initialize(this, getString(R.string.parse_application_id), getString(R.string.parse_client_secret));
+
+        saveUserToParse();
+    }
+
+    private void saveUserToParse() {
+        String uniqueString = UUID.randomUUID().toString().substring(0, 5);
+
+        ParseUser user = new ParseUser();
+        user.setUsername(uniqueString);
+        user.setPassword(uniqueString);
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+
+                } else {
+
+                }
+            }
+        });
     }
 }
